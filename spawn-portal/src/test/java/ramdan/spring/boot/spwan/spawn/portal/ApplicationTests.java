e2b849ce8224bac89c42397ff5735aec4d944a0d
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2020-05-24 01:11:32 +0700

    first commit AdminLTE 3

---
+++ spawn-portal/src/test/java/ramdan/spring/boot/spwan/spawn/portal/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ramdan.spring.boot.spwan.spawn.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
